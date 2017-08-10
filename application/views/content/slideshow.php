8bf7e9010da78ab3e361365b3ce91fd827a067b1
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-08-10 12:18:04 +0700

    fix logo and change default color :)

--- application/views/content/slideshow.php
+++ application/views/content/slideshow.php
@@ -1,8 +1,8 @@
 <div id="highlighted">
       <!-- Image Banner: Use the the data-bg-img functionality to create a simple image banner Use spacer classes to make the banner larger with padding, allows for easy responsive changes too see: elements-ctas.htm To make the banner full height/screen add the attributes: data-toggle="full-height" data-offset="#header" -->
-      <div class="bg-white overlay overlay-primary overlay-op-7 text-center px-3 py-5 py-lg-10 flex-valign bg-img" data-bg-img="<?=base_url();?>assets/img/slides/slide_1.jpg" data-css="{&quot;background-position&quot;: &quot;center bottom&quot;,&quot;background-attachment&quot;: &quot;fixed&quot;}" data-toggle="full-height" style="height: 421px; background-image: url(&quot;assets/img/backgrounds/young-man-standing-in-front-of-concrete-wall-picjumbo-com.jpg&quot;) !important; background-position: center bottom; background-attachment: fixed;">
+      <div class="bg-white overlay overlay-primary overlay-op-5 text-center px-3 py-5 py-lg-10 flex-valign bg-img" data-bg-img="<?=base_url();?>assets/img/slides/slide_1.jpg" data-css="{&quot;background-position&quot;: &quot;center bottom&quot;,&quot;background-attachment&quot;: &quot;fixed&quot;}" data-toggle="full-height" style="height: 421px; background-image: url(&quot;assets/img/backgrounds/young-man-standing-in-front-of-concrete-wall-picjumbo-com.jpg&quot;) !important; background-position: center bottom; background-attachment: fixed;">
         <h2 class="display-4 text-white animated fadeIn animated-done" data-animate="fadeIn" data-animate-delay="0.2" style="animation-delay: 0.2s;">
-          TYFI <span class="font-weight-bold">Indonesia!</span>
+          <span class="font-weight-bold">Tabernacle Youth Festival 2018!</span>
         </h2>
         <h3 class="text-grey font-weight-normal animated fadeIn animated-done" data-animate="fadeIn" data-animate-delay="0.35" style="animation-delay: 0.35s;">
           Your Slogan Here!
