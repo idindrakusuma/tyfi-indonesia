261a9ff95b1d44e5925041a50a257e97ebd19526
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-08-14 13:30:57 +0700

    update kontent gabung

--- application/views/content/gabung.php
+++ application/views/content/gabung.php
@@ -7,7 +7,7 @@
                 Tunggu apalagi ?
               </h3>
               <h5 class="mb-0">
-                Ayo ikuti INConference "Overflow" 2017 sekarang juga..</h5>
+                Ayo ikuti Tabernacle Youth Fellowship (TYF) Indonesia 2018!</h5>
             </div>
             <div class="col-12 col-lg-5 py-2 text-lg-right">
               <a href="<?=base_url();?>register" class="btn btn-lg btn-inverse btn-inverse-putih btn-invert px-4">Register Now <i class="fa fa-send ml-2"></i></a>
