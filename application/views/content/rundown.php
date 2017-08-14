cf79681727b912a08523aec05b83c231e8e3a523
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-08-14 13:28:24 +0700

    fix jadwal event

--- application/views/content/rundown.php
+++ application/views/content/rundown.php
@@ -6,7 +6,7 @@
               schedule
             </h2>
             <h3 class="text-center font-weight-light mt-2 mb-0 text-muted">
-              INConference 2017
+              TYF Indonesia 2018
             </h3>
             <hr class="mb-5 w-50 mx-auto" />
       </div>
@@ -18,30 +18,31 @@
                   <li class="nav-item">
                     <a href="#vtab-extended1" class="nav-link active" data-toggle="tab">
                       <span class="font-weight-bold">Hari Pertama</span> 
-                      <small>Jum'at, 1 September 2017 </small>
+                      <small>Selasa, 26 Juni 2018 </small>
                       <i class="fa fa-angle-right"></i> 
                     </a>
                   </li>
                   <li class="nav-item">
                     <a href="#vtab-extended2" class="nav-link" data-toggle="tab">
                       <span class="font-weight-bold">Hari Kedua</span> 
-                      <small>Sabtu, 2 September 2017 </small>
+                      <small>Rabu, 27 Juni 2018  </small>
                       <i class="fa fa-angle-right"></i> 
                     </a>
                   </li>
                   <li class="nav-item">
                     <a href="#vtab-extended3" class="nav-link" data-toggle="tab">
-                      <span class="font-weight-bold">Revival Night</span> 
-                      <small>Sabtu, 2 September 2017 </small>
+                      <span class="font-weight-bold">Hari Ketiga</span> 
+                      <small>Kamis, 28 Juni 2018  </small>
                       <i class="fa fa-angle-right"></i> 
                     </a>
                   </li>
                 </ul>
               </div>
               <div class="tab-content col-sm-9">
+								<!-- JADWAL HARI PERTAMA -->
                 <div class="tab-pane fade show active" id="vtab-extended1">
                   <h4>
-                    Hari Pertama, Lantai 6
+                    Hari Pertama, Selasa, 26 Juni 2018
                   </h4>
                   <table class="table table-bordered">
                   	<thead class="bg-merah">
@@ -54,91 +55,57 @@
                   	<tbody>
                   		<tr>
                   			<td>1</td>
-                  			<td>08.00 - 08.30</td>
+                  			<td>09.00 - 13.00</td>
                   			<td>Registration</td>
                   		</tr>
 
                   		<tr>
                   			<td>2</td>
-                  			<td>08.30 - 09.00</td>
-                  			<td>Preparation sound check</td>
+                  			<td>13.00 - 17.00</td>
+                  			<td>Istirahat + Mandi </td>
                   		</tr>
 
                   		<tr>
                   			<td>3</td>
-                  			<td>09.00 - 09.30</td>
-                  			<td>Opening games + Opening Worship Session + Opening video clip </td>
+                  			<td>17.00 - 18.00</td>
+                  			<td>Snack & Coffee Time</td>
                   		</tr>
 
                   		<tr>
                   			<td>4</td>
-                  			<td>09.30 - 10.00 </td>
-                  			<td>Keynote Speech: <b>"Overflow"</b> Ps. Yogi Santoso </td>
+                  			<td>18.00 - 18.30</td>
+                  			<td>Opening Games & Welcoming</td>
                   		</tr>
 
                   		<tr>
                   			<td>5</td>
-                  			<td>10.00 - 11.00</td>
-                  			<td>Workshop Session 1. Christopher Tapiheru: <b>"The Call"</b> </td>
+                  			<td>18.30 - 19.00</td>
+                  			<td>Dinner</td>
                   		</tr>
                   		<tr>
                   			<td>6</td>
-                  			<td>11.00 - 11.15 </td>
-                  			<td>Q & A Sesi 1</td>
+                  			<td>19.00 - 21.00 </td>
+                  			<td>Opening KKRVision Cast by Ps. Elijah Soetopo </td>
                   		</tr>
                   		<tr>
                   			<td>7</td>
-                  			<td>11.15 - 11.30 </td>
-                  			<td>Coffe Break</td>
+                  			<td>21.00 - 22.00 </td>
+                  			<td>Creative Group Battle games </td>
+											</tr>
+											<tr>
+                  			<td>8</td>
+                  			<td>22.00 </td>
+                  			<td>Istirahat</td>
                   		</tr>
-
-                  		<tr>
-                  			<td rowspan="2">8</td>
-                  			<td rowspan="2">11.30 - 12.30</td>
-                  			<td>Youth TALK : <b>"Youth with a mission"</b></td>
-                  		</tr>
-                      <tr>
-                        <td>Talkshow with GAC (Indonesian Artist) &Creative performance by GAC </td>
-                      </tr>
-
-                  		<tr>
-                  			<td>9</td>
-                  			<td>12.30 - 13.00 </td>
-                  			<td>Lunch</td>
-                  		</tr>
-                  		<tr>
-                  			<td>10</td>
-                  			<td>13.00 - 13.15 </td>
-                  			<td>Games breaker </td>
-                  		</tr>
-                  		<tr>
-                  			<td>11</td>
-                  			<td>13.15 - 14.15 </td>
-                  			<td>Workshop session 2. Victor Waang: <b>"Gen Z Discipleship"</b> </td>
-                  		</tr><tr>
-                  			<td>12</td>
-                  			<td>14.15 - 14.30 </td>
-                  			<td>Q & A Sesi 2</td>
-                  		</tr>
-                  		<tr>
-                  			<td>13</td>
-                  			<td>14.30 - 14.40</td>
-                  			<td>Creative Performance by GAC.
-								        </td>
-                  		</tr>
-                      <tr>
-                        <td>14</td>
-                        <td>14.40 - 15.30</td>
-                        <td>Youth TALK : <b>"Cinta dan Rahasia"</b> Christopher Tapiheru 
-                        </td>
-                      </tr>
                   	</tbody>
                   </table>
+								</div>
+								<!-- END OF JADWAL PERTAMA -->
 
-                </div>
+								<!-- JADWAL HARI KEDUA -->
                 <div class="tab-pane fade" id="vtab-extended2">
                   <h4>
-                    Hari Kedua, Lantai 6
+                    Hari Kedua, Rabu, 27 Juni 2018
                   </h4>
                   <table class="table table-bordered">
                   	<thead class="bg-merah">
@@ -151,95 +118,106 @@
                   	<tbody>
                   		<tr>
                   			<td>1</td>
-                  			<td>08.00 - 08.30 </td>
-                  			<td>Opening games + Opening Worship Session + Opening video clip.</td>
+                  			<td>06.00 - 08.00</td>
+                  			<td>Mandi & Breakfast</td>
                   		</tr>
                   		<tr>
-                  			<td>2</td>
-                  			<td>08.30 - 9.30 </td>
-                  			<td>Workshop Session 3 - Frangky Kuncoro : <b>"Build the atmosphere"</b></td>
-                  		</tr>
+                  			<td rowspan="2">2</td>
+                  			<td rowspan="2">08.00 - 10.00</td>
+                  			<td>Workshop 1 "Intuition" - Chris Tetuko</td>
+											</tr>
+											<tr>
+												<td>Sesi yang disampaikan dengan bermain boardgames.</td>
+											</tr>
                   		<tr>
                   			<td>3</td>
-                  			<td>9.30 - 9.45 </td>
-                  			<td>Q & A Sesi 3</td>
+                  			<td>10.00 - 10.15 </td>
+                  			<td>Snack & Coffee Time </td>
                   		</tr>
                   		<tr>
                   			<td>4</td>
-                  			<td>9.45 - 10.00</td>
-                  			<td>Coffe Break</td>
+                  			<td>10.15 - 12.00 </td>
+                  			<td>Workshop 2 "Indonesia Jangan Diam" - Andy Korinto </td>
                   		</tr>
                       <tr>
                         <td>5</td>
-                        <td>10.00 - 11.30</td>
-                        <td>Workshop session 4 - Yogi Santoso: <b>"Discipleship is Friendship"</b></td>
+                        <td>12.00 - 12.30</td>
+                        <td>Makan Siang</td>
                       </tr>
                       <tr>
                         <td>6</td>
-                        <td>11.30 - 11.45</td>
-                        <td>Q & A Sesi 4</b></td>
+                        <td>12.30 - 18.00 </td>
+                        <td>Jogja City Race </b></td>
                       </tr>
                   		<tr>
                   			<td>7</td>
-                  			<td>11.45 - 12.15</td>
-                  			<td>Lunch</td>
+                  			<td>18.00 - 19.00</td>
+                  			<td>Makan & Mandi </td>
                   		</tr>
                   		<tr>
                   			<td>8</td>
-                  			<td>12.15 - 13.15 </td>
-                  			<td>Workshop session 5. Victor Waang: <b>"TEAM Running"</b> </td>
+                  			<td>19.00 - 19.30</td>
+                  			<td>Dinner</td>
                   		</tr>
                       <tr>
                         <td>9</td>
-                        <td>13.15 - 13.30 </td>
-                        <td>Q & A Sesi 5 </td>
+                        <td>19.30</td>
+                        <td>Free Time - Discuss Group Explore Jogja </td>
                       </tr>
-                  		<tr>
-                  			<td>10</td>
-                  			<td>13.30 - 13.40</td>
-                  			<td>Creative performance by iGen</td>
-                  		</tr>
-
-                  		<tr>
-                  			<td>11</td>
-                  			<td>13.40 - 14.40 </td>
-                  			<td>Workshop session 6. Raditya Oloan: <b>"Creative digital evangelism"</b> </td>
-                  		</tr>
-
-                      <tr>
-                        <td>12</td>
-                        <td>14.40 - 14.55</td>
-                        <td>Q & A Sesi 6 </td>
-                      </tr>
-
-                      <tr>
-                        <td>13</td>
-                        <td>14.55 - 15.00 </td>
-                        <td>Closing</td>
-                      </tr>
+                  		
                   	</tbody>
                   </table>
                 </div>
+							<!-- END OF JADWAL HARI KEDUA -->
 
-                <!-- table revival night -->
+
+              <!-- START OF JADWAL HARI KETIGA -->
                 <div class="tab-pane fade" id="vtab-extended3">
                   <h4>
                     Revival Night, Lantai 1
                   </h4>
                   <table class="table table-bordered">
-                  	<thead class="bg-merah">
-                  		<tr>
-                  			<th>Revival Night</th>
-                  		</tr>
-                  	</thead>
-                  	<tbody>
-                  	   <tr>
-                        <td>Guest Star: Frangky Kuncoro</td> 
-                       </tr>
-                  	</tbody>
+										<thead class="bg-merah">
+											<tr>
+												<th>No</th>
+												<th>Time</th>
+												<th>Activity</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>1</td>
+												<td>06.00 - 08.00</td>
+												<td>Mandi & Breakfast</td>
+											</tr>
+
+											<tr>
+												<td>2</td>
+												<td>08.00 - 10.00</td>
+												<td>Lypsinc Battle </td>
+											</tr>
+
+											<tr>
+												<td>3</td>
+												<td>10.00 - 10.30</td>
+												<td>Snack & Coffee Time</td>
+											</tr>
+
+											<tr>
+												<td>4</td>
+												<td>10.30 - 11.00 </td>
+												<td>Pembagian Hadiah & Closing </td>
+											</tr>
+
+											<tr>
+												<td>5</td>
+												<td>11.00 </td>
+												<td>Makan siang & Check Out & Pulang</td>
+											</tr>
+											</tbody>
                   </table>
                 </div>
-
+							<!-- END OF JADWAL HARI KETIGA -->
               </div>
             </div>
     	</div>
