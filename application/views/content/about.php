7889d51a40137375673d7b5ea5ba44a3d57dde0b
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-08-14 11:55:52 +0700

    add deskripsi (- gambar dan tema)

--- application/views/content/about.php
+++ application/views/content/about.php
@@ -6,7 +6,7 @@
               About TYF Indonesia 2017
             </h2>
             <h3 class="text-center font-weight-light mt-2 mb-0 text-muted">
-               "Overflow"
+               "Isi Temanya Disini"
             </h3>
             <hr class="mb-5 w-50 mx-auto" />
           </div>
@@ -17,7 +17,7 @@
               
             </div>
             <div class="col-md-6" data-animate="fadeIn" data-animate-delay="0.8" data-animate-offset="100%">
-              <p class="mb-0">Nulla excepteur et deserunt voluptate. Aliqua cillum eu ullamco irure ex aliqua nulla ipsum. Ut consectetur amet nulla Lorem sint. Non excepteur ad ut amet cupidatat ex dolor deserunt adipisicing deserunt excepteur. Velit culpa consectetur pariatur elit labore magna tempor laboris irure. Veniam dolor est sint in do ipsum. Veniam irure exercitation commodo minim exercitation.</p><br>
+              <p class="mb-0">Tabernacle Youth Fellowship (TYF) mulai terbentuk sejak 2009 dan mulai aktif melakukan kegiatan event nasional yang berawal di Surabaya. Selanjutnya TYF terus melakukan kegiatan nasional tahunan dengan berpindah kota dari daerah Jawa Barat, Jawa Tengah dan Jawa Timur. Ratusan anak muda telah terlibat dalam kepanitiaan dan ribuan anak muda dari berbagai daerah di Indonesia juga sudah mendapatkan sebuah dampak dari kegerakan TYF ini.</p><br>
               <button class="btn btn-inverse btn-invert b-1-em bg-merah" data-toggle="modal" data-target=".about-inconference" href="#">Selengkapnya</button><br/>
             </div>
           </div>
--- application/views/content/modal/m_about.php
+++ application/views/content/modal/m_about.php
@@ -5,17 +5,16 @@
                 <div class="modal-content">
                   <div class="modal-header">
                     <h4 class="modal-title">
-                      Inconference 2017 "Overflow"
+                    About TYFIndonesia
                     </h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
                   </div>
                   <div class="modal-body">
-                    Tahun 2014-2016 adalah waktu di mana kita kembali menemukan dan memperkuat sebuah dasar filosofi pelayanan yang akhirnya menjadi identitas dan jatidiri komunitas. Dengan sebuah vision statement "Inspiration For Everyone" maka komunitas pemuda remaja di gereja ini memiliki sebuah kerinduan untuk dapat menginpirasi generasi ini dengan pesan kebenaran. Tiga tahun terakhir juga menjadi sebuah proses waktu dimana beberapa anak muda memberikan diri lebih jauh untuk melayani generasi dengan sebuah komitmen yang kuat dan akhirnya menjadi satu team yang cukup solid. Maka tahun 2014-2016 menjadi tahun penguatan kembali akan visi dan identitas dan juga membangun sebuah dasar team yang lebih solid dari sebelumnya. <br><br>
-                    Masuk di tahun 2017 kami mendapatkan sebuah visi yang satu langkah lebih maju dari tiga tahun sebelumnya yaitu ingin menginspirasi kota. Maka lahirlah sebuah ide dan hati untuk mengadakan sebuah Youth Conference bagi para pemimpin anak muda di kota Semarang dan juga bagi mereka yang terbeban untuk melayani generasi muda agar bisa diperlengkapi. Conference ini ada untuk membantu gereja-gereja di kota Semarang supaya bisa melayani generasi muda dengan lebih efektif dan relevan dan juga untuk membagikan api kegerakan bagi anak muda. Ini adalah tempat untuk saling mengenal, belajar dan berbagi.<br><br>
-                    Conference ini juga akan menjadi sebuah alat bagi komunitas kita untuk berbagi dengan gereja-gereja atau komunitas-komunitas lain di kota Semarang dan sekitarnya. Jadi ada hati untuk memberi melalui sebuah conference. Sebagai bagian dari tubuh Kristus di kota ini maka kita juga wajib untuk ikut ambil bagian dalam kemajuan gereja-gereja lain melalui pelayanan anak muda mereka.
+                  Tabernacle Youth Fellowship (TYF) mulai terbentuk sejak 2009 dan mulai aktif melakukan kegiatan event nasional yang berawal di Surabaya. Selanjutnya TYF terus melakukan kegiatan nasional tahunan dengan berpindah kota dari daerah Jawa Barat, Jawa Tengah dan Jawa Timur. Ratusan anak muda telah terlibat dalam kepanitiaan dan ribuan anak muda dari berbagai daerah di Indonesia juga sudah mendapatkan sebuah dampak dari kegerakan TYF ini. TYF terus mengalami banyak pertumbuhan dan perkembangan hingga saat ini dengan adanya regenerasi kepemimpinan yang ada. TYF terus mencoba menjawab tantangan pelayanan anak muda secara relevan pada masa ini dan ingin terus menjaga pergerakan bahkan berdoa untuk sesuatu yang lebih besar.<br><br>
+                  Selama tahun 2009 - 2016 ini TYF telah melakukan kegiatan tahunan secara konsisten dengan sebuah format kelas workshop yang membahas misi 5P (Penjangkauan, Pemuridan, Persekutuan, Pelayanan dan Penyembahan) dan KKR. Melihat generasi yang terus berubah dan perkembangan zaman maka di tahun 2018 TYF ingin sedikit melakukan pendekatan yang berbeda yaitu dengan melakukan experiental learning. Dengan pendekatan ini diharapkan TYF dapat secara efektif dan relevan memberikan pesan kebenaran dan memasukkan misi 5P kepada setiap peserta.
                   </div>
                   <div class="modal-footer">
-                    <button type="button" class="btn btn-inverse btn-invert" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-inverse bg-merah btn-invert" data-dismiss="modal">Close</button>
                   </div>
                 </div>
               </div>
