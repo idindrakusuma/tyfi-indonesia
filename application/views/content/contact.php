04e17e3ff7d76135324839f30b9b8c9a99d6831c
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-08-14 11:44:31 +0700

    fix info contact us

--- application/views/content/contact.php
+++ application/views/content/contact.php
@@ -14,7 +14,7 @@
           <div class="row" data-animate="fadeIn" data-animate-delay="0.1">
             <div class="col-md-12 mb-5">
               <div class="google-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.1798073990703!2d110.42057401421222!3d-6.988076270375538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b59b010f751%3A0xff4f70e80333bbb!2sJl.+Gajahmada+No.114%2C+Pekunden%2C+Semarang+Tengah%2C+Kota+Semarang%2C+Jawa+Tengah+50241!5e0!3m2!1sid!2sid!4v1495783501569" width="600" height="450" frameborder="0" style="border:0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7920.496546122318!2d110.42146300000002!3d-6.980004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1ba1bc11f985c113!2sGereja+Bethel+Tabernakel+Kristus+Alfa+Omega!5e0!3m2!1sen!2sid!4v1502685743254" width="600" height="450" frameborder="0" style="border:0"></iframe>
               </div>
             </div>
             <div class="col-md-6">
@@ -47,27 +47,26 @@
             </div>
             <div class="col-md-5 offset-md-1 mt-4 mt-md-0" data-animate="fadeIn" data-animate-delay="0.4">
               <h4 class="mb-1 font-weight-bold">
-                INConference 2017
+              TYF Indonesia
               </h4>
               <p class="mb-1">
                 <abbr title="Address"><i class="fa fa-home pr-2"></i></abbr>
-                Jalan Gajahmada no 114 - 118 - 50138, Semarang
+                Gajah Mada 114-118 Street, Semarang, 50138 
               </p>
               <p class="mb-1">
                 <abbr title="Telpon"><i class="fa fa-phone pr-2"></i></abbr>
-                08170597799
+                081362171185
               </p>
                <p class="mb-1">
-                <abbr title="Telpon"><i class="fa fa-instagram pr-2"></i></abbr>
-                @igen.youth | @igen.youngadult | @igen.teens
+                <abbr title="Telpon"><i class="fa fa-envelope pr-2"></i></abbr>
+                tyfindonesia@gmail.com
               </p>
               <p class="mb-0">
                 <abbr title="Website"><i class="fa fa-globe pr-2"></i></abbr>
-                www.geninspiration.com
+                tyfindonesia.com
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-    
